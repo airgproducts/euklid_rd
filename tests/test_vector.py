920ae57d02182d1826be004b6c167ece3fe3887f
hiaselhans <simon.klemenc@gmail.com>
2022-01-09 23:46:47 +0100

    use nalgebra library
    - switch to use getitem/setitem instead of x/y
    - use nalgebra vector2 internally

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -8,30 +8,38 @@ import unittest
 from euklid_rs.vector import Vector2D
 
 class TestRustModules(unittest.TestCase):
-    '''Test euklid_rs rust module'''  
+    '''Test euklid_rs rust module'''
+    def setUp(self) -> None:
+        self.p1 = Vector2D([0,0])
+        self.p2 = Vector2D([1,1])
+        self.p3 = Vector2D([2,3])
     
     def test_vector2d_init_values(self):
         '''test_vector2d_init_values'''
-        p1 = Vector2D([0, 0])
-        self.assertIs(type(p1), Vector2D)
-        self.assertEqual(p1.x, 0.0)
-        self.assertEqual(p1.y, 0.0)
-        self.assertEqual(str(p1), 'Vector2D(0.0000 0.0000)')
+        self.assertIs(type(self.p1), Vector2D)
+        self.assertEqual(self.p1[0], 0.0)
+        self.assertEqual(self.p1[1], 0.0)
+        self.assertEqual(str(self.p1), 'Vector2D(0.0000 0.0000)')
+    
+    def test_norm(self):
+        self.assertAlmostEqual(self.p1.length(), 0)
+        self.assertAlmostEqual(self.p2.length(), math.sqrt(2))
+        self.assertAlmostEqual(self.p3.length(), math.sqrt(13))
 
     def test_vector2d_normalized_values(self):
         '''test_vector2d_normalized_values checks if the values'''
-        p1 = Vector2D([1, 1])
+        self.assertAlmostEqual(self.p2.normalized().length(), 1)
+        self.assertAlmostEqual(self.p3.normalized().length(), 1)
 
-        self.assertEqual(str(p1.normalized()), 'Vector2D(0.7071 0.7071)')
+        self.assertTrue(math.isnan(self.p1.normalized()[0]))
+        self.assertTrue(math.isnan(self.p1.normalized()[1]))
 
-        p2 = Vector2D([0, 0])
-        self.assertTrue(math.isnan(p2.normalized().x))
-        self.assertTrue(math.isnan(p2.normalized().y))
-
-
-        p3 = Vector2D([2, 3])
-        self.assertEqual(round(p3.normalized().x, 4), 0.5547)
-        self.assertEqual(round(p3.normalized().y, 4), 0.8321)
+        self.assertEqual(round(self.p3.normalized()[0], 4), 0.5547)
+        self.assertEqual(round(self.p3.normalized()[1], 4), 0.8321)
+    
+    def test_access_invalid(self):
+        self.assertRaises(IndexError, lambda: self.p1[2])
+        self.assertRaises(IndexError, lambda: self.p1[-1])
 
 if __name__ == '__main__':
     unittest.main(exit=False)
