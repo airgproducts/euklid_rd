38f4ae424207a0dac22b58d23a9482b09d912b78
Daniel Naschberger <daniel@naschi.at>
2022-01-10 19:51:22 +0100

    chore(vector_rs): simplify

--- src/vector_rs.rs
+++ src/vector_rs.rs
@@ -9,7 +9,7 @@ use nalgebra as na;
 #[pyclass]
 #[derive(Clone, Copy)]
 struct Vector2D {
-    vec: na::Vector2<f64>
+    values: na::Vector2<f64>,
 }
 
 
@@ -17,66 +17,68 @@ struct Vector2D {
 impl Vector2D {
     #[new]
     pub fn __new__(v: [f64; 2]) -> Self {
-        Vector2D {vec: na::Vector2::new(v[0], v[1])}
+        let values =  na::Vector2::new(v[0], v[1]);
+        Vector2D {values}
     }
     
     pub fn normalized(&self) -> PyResult<Self> {
-        let length = self.vec.norm();
-
-        return Ok(Vector2D {vec: self.vec / length});
+        let values = self.values / self.values.norm();
+        Ok(Vector2D {values})
     }
 
     pub fn length(&self) -> PyResult<f64> {
-        return Ok(self.vec.norm());
+        Ok(self.values.norm())
     }
 }
 
 #[pyproto]
 impl PyObjectProtocol for Vector2D {
     fn __repr__(&self) -> PyResult<String> {
-        Ok(format!("Vector2D({:.4} {:.4})", self.vec[0], self.vec[1]))
+        Ok(format!("Vector2D({:.4} {:.4})", self.values[0], self.values[1]))
     }
 
 }
 
 #[pyproto]
 impl PyNumberProtocol for Vector2D {
-    fn __add__(left: Self, other: Self) -> PyResult<Self> {
-        return Ok(Self {vec: left.vec + other.vec });
+    fn __add__(lhs: Self, rhs: Self) -> PyResult<Self> {
+        Ok(Self {values: lhs.values + rhs.values })
    }
 
    fn __sub__(lhs: Self, rhs: Self) -> PyResult<Self> {
-       return Ok(Self {vec: lhs.vec - rhs.vec })
+       Ok(Vector2D {values: lhs.values - rhs.values })
    }
 
-   fn __mul__(lhs: Self, rhs: f64) -> PyResult<Self> {
-       return Ok(Self {vec: lhs.vec * rhs});
+   fn __mul__(lhs: Self, value: f64) -> PyResult<Self> {
+       Ok(Vector2D {values: lhs.values * value})
    }
 
-   fn __truediv__(lhs: Self, rhs: f64) -> PyResult<Self> {
-       return Ok(Self {vec: lhs.vec / rhs});
+   fn __truediv__(lhs: Self, value: f64) -> PyResult<Self> {
+       Ok(Vector2D {values: lhs.values / value})
    }
 }
 
 #[pyproto]
 impl PySequenceProtocol for Vector2D {
     fn __getitem__(&self, idx: isize) -> PyResult<f64> {
-        if idx < 0 || idx > 1 {
-            return Err(PyIndexError::new_err("index out of range"));
-        }
-        let n_us = usize::try_from(idx).unwrap();
-        return Ok(self.vec[n_us]);
-        
+        match idx {
+            0 | 1 => {
+                let n_us = usize::try_from(idx).unwrap();
+                Ok(self.values[n_us])
+            }
+            _ => Err(PyIndexError::new_err("index out of range"))
+        }        
     }
 
     fn __setitem__(&mut self, idx: isize, value: f64) -> PyResult<()> {
-        if idx < 0 || idx > 1 {
-            return Err(PyIndexError::new_err("index out of range"));
+        match idx {
+            0 | 1 => {
+                let n_us = usize::try_from(idx).unwrap();
+                self.values[n_us] = value;
+                Ok(())
+            }
+            _ => Err(PyIndexError::new_err("index out of range"))
         }
-        let n_us = usize::try_from(idx).unwrap();
-        self.vec[n_us] = value;
-        
-        return Ok({});
     }
 }
 
--- tests/test_vector.py
+++ tests/test_vector.py
@@ -13,15 +13,16 @@ class TestRustModules(unittest.TestCase):
         self.p1 = Vector2D([0,0])
         self.p2 = Vector2D([1,1])
         self.p3 = Vector2D([2,3])
-    
+
     def test_vector2d_init_values(self):
         '''test_vector2d_init_values'''
         self.assertIs(type(self.p1), Vector2D)
         self.assertEqual(self.p1[0], 0.0)
         self.assertEqual(self.p1[1], 0.0)
         self.assertEqual(str(self.p1), 'Vector2D(0.0000 0.0000)')
-    
-    def test_norm(self):
+
+    def test_length(self):
+        '''test_length get the length of a vector'''
         self.assertAlmostEqual(self.p1.length(), 0)
         self.assertAlmostEqual(self.p2.length(), math.sqrt(2))
         self.assertAlmostEqual(self.p3.length(), math.sqrt(13))
@@ -36,10 +37,32 @@ class TestRustModules(unittest.TestCase):
 
         self.assertEqual(round(self.p3.normalized()[0], 4), 0.5547)
         self.assertEqual(round(self.p3.normalized()[1], 4), 0.8321)
-    
+
     def test_access_invalid(self):
+        '''test_access_invalid raises a error'''
         self.assertRaises(IndexError, lambda: self.p1[2])
         self.assertRaises(IndexError, lambda: self.p1[-1])
+
+    def test__setitem__(self):
+        '''test__setitem__ sets a value at a position'''
+        self.p1[0] = 2
+        self.assertEqual(self.p1[0], 2.)
+
+    def test__add__(self):
+        '''test__add__ tries to add two vectors'''
+        self.assertEqual(str(self.p2 + self.p3), 'Vector2D(3.0000 4.0000)')
+
+    def test__sub__(self):
+        '''test__add__ tries to subtract two vectors'''
+        self.assertEqual(str(self.p2 - self.p3), 'Vector2D(-1.0000 -2.0000)')
+
+    def test__mul__(self):
+        '''test__mul__ multiplies a vector'''
+        self.assertEqual(str(self.p3 * 3), 'Vector2D(6.0000 9.0000)')
+
+    def test__truediv__(self):
+        '''test__truediv__ devides a vector'''
+        self.assertEqual(str(self.p3 / 2), 'Vector2D(1.0000 1.5000)')
 
 if __name__ == '__main__':
     unittest.main(exit=False)
