8c5e8bd651e45e39bd8321e7b10a4d2d5e66bd96
Daniel Naschberger <daniel@naschi.at>
2022-01-22 10:21:09 +0100

    chore(vector): macro for vector2d and vector3d

--- src/vector.rs
+++ src/vector.rs
@@ -17,7 +17,6 @@ trait Vector {
     fn dot(&self, other: &Self) -> f64;
     fn length(&self) -> f64;
     fn normalized(&self) -> Self;
-    fn __repr__(&self) -> String;
 }
 
 macro_rules! vector{($src: ident, $dst: ident) => {
@@ -53,12 +52,70 @@ macro_rules! vector{($src: ident, $dst: ident) => {
             let v = self.v / self.v.norm();
             Self {v}
         }
+    }
 
-        fn __repr__(&self) -> String {
-            match self.v.len() {
+    #[pyproto]
+    impl PyObjectProtocol for $dst {
+        fn __repr__(&self) -> PyResult<String> {
+            let temp_string = match self.v.len() {
                 2 => format!("Vector2D({:.4} {:.4})", self.v[0], self.v[1]),
                 3 => format!("Vector3D({:.4} {:.4} {:.4})", self.v[0], self.v[1], self.v[2]),
                 _ => panic!("Not implemented")
+            };
+            Ok(temp_string)
+        }
+
+        fn __richcmp__(&'p self, other: PyRef<'p, Vector2D>, op: pyo3::basic::CompareOp) -> PyResult<bool> {
+            match op {
+                pyo3::basic::CompareOp::Eq => Ok(self.v == other.v),
+                pyo3::basic::CompareOp::Lt => Ok(self.length() < other.length()),
+                pyo3::basic::CompareOp::Le => Ok(self.length() <= other.length()),
+                pyo3::basic::CompareOp::Gt => Ok(self.length() > other.length()),
+                pyo3::basic::CompareOp::Ge => Ok(self.length() >= other.length()),
+                _ => Err(PyNotImplementedError::new_err("Not Implemented")),
+            }
+        }
+    }
+
+    #[pyproto]
+    impl PyNumberProtocol for $dst {
+        fn __add__(lhs: Self, rhs: Self) -> PyResult<Self> {
+            Ok(Self {v: lhs.v + rhs.v })
+    }
+
+    fn __sub__(lhs: Self, rhs: Self) -> PyResult<Self> {
+        Ok(Self {v: lhs.v - rhs.v })
+    }
+
+    fn __mul__(lhs: Self, value: f64) -> PyResult<Self> {
+        Ok(Self {v: lhs.v * value})
+    }
+
+    fn __truediv__(lhs: Self, value: f64) -> PyResult<Self> {
+        Ok(Self {v: lhs.v / value})
+    }
+    }
+
+    #[pyproto]
+    impl PySequenceProtocol for $dst {
+        fn __getitem__(&self, idx: isize) -> PyResult<f64> {
+            match idx {
+                0 | 1 => {
+                    let n_us = usize::try_from(idx).unwrap();
+                    Ok(self.v[n_us])
+                }
+                _ => Err(PyIndexError::new_err("index out of range"))
+            }
+        }
+
+        fn __setitem__(&mut self, idx: isize, value: f64) -> PyResult<()> {
+            match idx {
+                0 | 1 => {
+                    let n_us = usize::try_from(idx).unwrap();
+                    self.v[n_us] = value;
+                    Ok(())
+                }
+                _ => Err(PyIndexError::new_err("index out of range"))
             }
         }
     }
@@ -160,77 +217,6 @@ impl Vector3D {
         Vector::normalized(&self)
     }
 }
-
-#[pyproto]
-impl PyObjectProtocol for Vector2D {
-    fn __repr__(&self) -> PyResult<String> {
-        Ok(Vector::__repr__(self))
-    }
-
-    fn __richcmp__(&'p self, other: PyRef<'p, Vector2D>, op: pyo3::basic::CompareOp) -> PyResult<bool> {
-        match op {
-            pyo3::basic::CompareOp::Eq => Ok(self.v == other.v),
-            pyo3::basic::CompareOp::Lt => Ok(self.length() < other.length()),
-            pyo3::basic::CompareOp::Le => Ok(self.length() <= other.length()),
-            pyo3::basic::CompareOp::Gt => Ok(self.length() > other.length()),
-            pyo3::basic::CompareOp::Ge => Ok(self.length() >= other.length()),
-            _ => Err(PyNotImplementedError::new_err("Not Implemented")),
-        }
-    }
-
-}
-
-#[pyproto]
-impl PyObjectProtocol for Vector3D {
-    fn __repr__(&self) -> PyResult<String> {
-        Ok(Vector::__repr__(self))
-    }
-}
-
-#[pyproto]
-impl PyNumberProtocol for Vector2D {
-    fn __add__(lhs: Self, rhs: Self) -> PyResult<Self> {
-        Ok(Self {v: lhs.v + rhs.v })
-   }
-
-   fn __sub__(lhs: Self, rhs: Self) -> PyResult<Self> {
-       Ok(Self {v: lhs.v - rhs.v })
-   }
-
-   fn __mul__(lhs: Self, value: f64) -> PyResult<Self> {
-       Ok(Self {v: lhs.v * value})
-   }
-
-   fn __truediv__(lhs: Self, value: f64) -> PyResult<Self> {
-       Ok(Self {v: lhs.v / value})
-   }
-}
-
-#[pyproto]
-impl PySequenceProtocol for Vector2D {
-    fn __getitem__(&self, idx: isize) -> PyResult<f64> {
-        match idx {
-            0 | 1 => {
-                let n_us = usize::try_from(idx).unwrap();
-                Ok(self.v[n_us])
-            }
-            _ => Err(PyIndexError::new_err("index out of range"))
-        }
-    }
-
-    fn __setitem__(&mut self, idx: isize, value: f64) -> PyResult<()> {
-        match idx {
-            0 | 1 => {
-                let n_us = usize::try_from(idx).unwrap();
-                self.v[n_us] = value;
-                Ok(())
-            }
-            _ => Err(PyIndexError::new_err("index out of range"))
-        }
-    }
-}
-
-
 
 pub fn register(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pymodule]
