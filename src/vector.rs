34a1aacf3b0fba28f03b79cd2189971291388f3a
Daniel Naschberger <daniel@naschi.at>
2022-01-25 18:34:54 +0100

    chore: cargo-fmt

--- src/vector.rs
+++ src/vector.rs
@@ -1,14 +1,13 @@
-use std::convert::TryFrom;
-use pyo3::prelude::*;
-use pyo3::exceptions::PyIndexError;
-use pyo3::exceptions::PyNotImplementedError;
+use nalgebra as na;
+use pyo3::class::basic::PyObjectProtocol;
 use pyo3::class::number::PyNumberProtocol;
 use pyo3::class::sequence::PySequenceProtocol;
-use pyo3::class::basic::PyObjectProtocol;
+use pyo3::exceptions::PyIndexError;
+use pyo3::exceptions::PyNotImplementedError;
+use pyo3::prelude::*;
 use pyo3::types::PyDict;
 use pyo3::wrap_pymodule;
-use nalgebra as na;
-
+use std::convert::TryFrom;
 
 trait Vector {
     fn angle(&self) -> f64;
@@ -18,102 +17,111 @@ trait Vector {
     fn normalized(&self) -> Self;
 }
 
-macro_rules! vector{($src: ident, $dst: ident) => {
-    impl $src for $dst {
-        fn angle(&self) -> f64 {
-            match self.v.len() {
-                2 => f64::atan2(self.v[1], self.v[0]),
-                _ => panic!("Not Implemented")
-            }
-        }
-
-        fn copy(&self) -> Self {
-            let v = self.v.clone();
-            Self {v}
-        }
-
-        fn dot(&self, other: &Self) -> f64 {
-            self.v.dot(&other.v)
-        }
-
-        fn length(&self) -> f64 {
-            self.v.norm()
-        }
-
-        fn normalized(&self) -> Self {
-            let v = self.v / self.v.norm();
-            Self {v}
-        }
-    }
-
-    #[pyproto]
-    impl PyObjectProtocol for $dst {
-        fn __repr__(&self) -> PyResult<String> {
-            let temp_string = match self.v.len() {
-                2 => format!("Vector2D({:.4} {:.4})", self.v[0], self.v[1]),
-                3 => format!("Vector3D({:.4} {:.4} {:.4})", self.v[0], self.v[1], self.v[2]),
-                _ => panic!("Not implemented")
-            };
-            Ok(temp_string)
-        }
-
-        fn __richcmp__(&'p self, other: PyRef<'p, $dst>, op: pyo3::basic::CompareOp) -> PyResult<bool> {
-            match op {
-                pyo3::basic::CompareOp::Eq => Ok(self.v == other.v),
-                pyo3::basic::CompareOp::Lt => Ok(self.length() < other.length()),
-                pyo3::basic::CompareOp::Le => Ok(self.length() <= other.length()),
-                pyo3::basic::CompareOp::Gt => Ok(self.length() > other.length()),
-                pyo3::basic::CompareOp::Ge => Ok(self.length() >= other.length()),
-                _ => Err(PyNotImplementedError::new_err("Not Implemented")),
-            }
-        }
-    }
-
-    #[pyproto]
-    impl PyNumberProtocol for $dst {
-        fn __add__(lhs: Self, rhs: Self) -> PyResult<Self> {
-            Ok(Self {v: lhs.v + rhs.v })
-    }
-
-    fn __sub__(lhs: Self, rhs: Self) -> PyResult<Self> {
-        Ok(Self {v: lhs.v - rhs.v })
-    }
-
-    fn __mul__(lhs: Self, value: f64) -> PyResult<Self> {
-        Ok(Self {v: lhs.v * value})
-    }
-
-    fn __truediv__(lhs: Self, value: f64) -> PyResult<Self> {
-        Ok(Self {v: lhs.v / value})
-    }
-    }
-
-    #[pyproto]
-    impl PySequenceProtocol for $dst {
-        fn __getitem__(&self, idx: isize) -> PyResult<f64> {
-            match idx {
-                0 | 1 => {
-                    let n_us = usize::try_from(idx).unwrap();
-                    Ok(self.v[n_us])
+macro_rules! vector {
+    ($src: ident, $dst: ident) => {
+        impl $src for $dst {
+            fn angle(&self) -> f64 {
+                match self.v.len() {
+                    2 => f64::atan2(self.v[1], self.v[0]),
+                    _ => panic!("Not Implemented"),
                 }
-                _ => Err(PyIndexError::new_err("index out of range"))
+            }
+
+            fn copy(&self) -> Self {
+                let v = self.v.clone();
+                Self { v }
+            }
+
+            fn dot(&self, other: &Self) -> f64 {
+                self.v.dot(&other.v)
+            }
+
+            fn length(&self) -> f64 {
+                self.v.norm()
+            }
+
+            fn normalized(&self) -> Self {
+                let v = self.v / self.v.norm();
+                Self { v }
             }
         }
 
-        fn __setitem__(&mut self, idx: isize, value: f64) -> PyResult<()> {
-            struct S(usize, isize);
-            let _v_len = self.v.len();
-            match S(_v_len, idx) {
-                S(2, 0 ..= 2) | S(3, 0 ..= 3) => {
-                    let n_us = usize::try_from(idx).unwrap();
-                    self.v[n_us] = value;
-                    Ok(())
+        #[pyproto]
+        impl PyObjectProtocol for $dst {
+            fn __repr__(&self) -> PyResult<String> {
+                let temp_string = match self.v.len() {
+                    2 => format!("Vector2D({:.4} {:.4})", self.v[0], self.v[1]),
+                    3 => format!(
+                        "Vector3D({:.4} {:.4} {:.4})",
+                        self.v[0], self.v[1], self.v[2]
+                    ),
+                    _ => panic!("Not implemented"),
+                };
+                Ok(temp_string)
+            }
+
+            fn __richcmp__(
+                &'p self,
+                other: PyRef<'p, $dst>,
+                op: pyo3::basic::CompareOp,
+            ) -> PyResult<bool> {
+                match op {
+                    pyo3::basic::CompareOp::Eq => Ok(self.v == other.v),
+                    pyo3::basic::CompareOp::Lt => Ok(self.length() < other.length()),
+                    pyo3::basic::CompareOp::Le => Ok(self.length() <= other.length()),
+                    pyo3::basic::CompareOp::Gt => Ok(self.length() > other.length()),
+                    pyo3::basic::CompareOp::Ge => Ok(self.length() >= other.length()),
+                    _ => Err(PyNotImplementedError::new_err("Not Implemented")),
                 }
-                _ => Err(PyIndexError::new_err("index out of range"))
             }
         }
-    }
-}}
+
+        #[pyproto]
+        impl PyNumberProtocol for $dst {
+            fn __add__(lhs: Self, rhs: Self) -> PyResult<Self> {
+                Ok(Self { v: lhs.v + rhs.v })
+            }
+
+            fn __sub__(lhs: Self, rhs: Self) -> PyResult<Self> {
+                Ok(Self { v: lhs.v - rhs.v })
+            }
+
+            fn __mul__(lhs: Self, value: f64) -> PyResult<Self> {
+                Ok(Self { v: lhs.v * value })
+            }
+
+            fn __truediv__(lhs: Self, value: f64) -> PyResult<Self> {
+                Ok(Self { v: lhs.v / value })
+            }
+        }
+
+        #[pyproto]
+        impl PySequenceProtocol for $dst {
+            fn __getitem__(&self, idx: isize) -> PyResult<f64> {
+                match idx {
+                    0 | 1 => {
+                        let n_us = usize::try_from(idx).unwrap();
+                        Ok(self.v[n_us])
+                    }
+                    _ => Err(PyIndexError::new_err("index out of range")),
+                }
+            }
+
+            fn __setitem__(&mut self, idx: isize, value: f64) -> PyResult<()> {
+                struct S(usize, isize);
+                let _v_len = self.v.len();
+                match S(_v_len, idx) {
+                    S(2, 0..=2) | S(3, 0..=3) => {
+                        let n_us = usize::try_from(idx).unwrap();
+                        self.v[n_us] = value;
+                        Ok(())
+                    }
+                    _ => Err(PyIndexError::new_err("index out of range")),
+                }
+            }
+        }
+    };
+}
 
 vector!(Vector, Vector2D);
 vector!(Vector, Vector3D);
@@ -135,7 +143,7 @@ impl Vector2D {
     #[new]
     fn __new__(v: [f64; 2]) -> PyResult<Self> {
         let v = na::Vector2::new(v[0], v[1]);
-        Ok(Self {v})
+        Ok(Self { v })
     }
 
     /// angle($self)
@@ -145,7 +153,7 @@ impl Vector2D {
     fn angle(&self) -> f64 {
         Vector::angle(self)
     }
-    
+
     /// copy($self)
     /// --
     ///
@@ -192,7 +200,7 @@ impl Vector3D {
     #[new]
     fn __new__(v: [f64; 3]) -> PyResult<Self> {
         let v = na::Vector3::new(v[0], v[1], v[2]);
-        Ok(Self {v})
+        Ok(Self { v })
     }
 
     /// angle($self)
@@ -202,7 +210,7 @@ impl Vector3D {
     fn angle(&self) -> f64 {
         Vector::angle(self)
     }
-    
+
     /// copy($self)
     /// --
     ///
